be8478100437626088bffa5b4f333d4d5243065c
SUH SAECHAN <83532821+Cassiiopeia@users.noreply.github.com>
2025-10-12 00:56:52 +0900

    이미 진행된 프로젝트에 대해서 바로 템플릿을 도입할 수 있는 자동화 기능 제공 필요 : fix : 색상코드 제거   https://github.com/Cassiiopeia/SUH-DEVOPS-TEMPLATE/issues/74

--- template_integrator.sh
+++ template_integrator.sh
@@ -89,14 +89,14 @@ detect_terminal() {
     fi
 }
 
-# 색상 정의
-RED='\033[0;31m'
-GREEN='\033[0;32m'
-YELLOW='\033[1;33m'
-BLUE='\033[0;34m'
-CYAN='\033[0;36m'
-MAGENTA='\033[0;35m'
-NC='\033[0m' # No Color
+# 색상 정의 (비활성화 - 안정성 향상)
+RED=''
+GREEN=''
+YELLOW=''
+BLUE=''
+CYAN=''
+MAGENTA=''
+NC=''
 
 # 템플릿 저장소 URL
 TEMPLATE_REPO="https://github.com/Cassiiopeia/SUH-DEVOPS-TEMPLATE.git"
